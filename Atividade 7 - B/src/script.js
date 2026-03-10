4cdb018c6f37e5c18f6ae3f6c29e8b6a3bff9554
Matheus <matheusmatos463463@gmail.com>
2026-03-10 15:26:42 -0300

    B - °F em °C

--- Atividade 7 - B/src/script.js
+++ Atividade 7 - B/src/script.js
@@ -1,1 +1,6 @@
 // Ler uma temperatura em graus Fahrenheit e apresentá-la convertida em graus Celsius. A fórmula de conversão é C  (F - 32) * (5/9) , sendo F a temperatura em Fahrenheit e C a temperatura em Celsius.
+
+let tempFahrenheit = parseInt(prompt("Digite a temperatura em Fahrenheit(°F):")); //Lê e armazena o que o usuário digitar
+let valorCelsius = (tempFahrenheit - 32) * (5/9); //Fórmula que converte °F para °C com base no valor de "tempFahrenheit"
+
+document.querySelector(".text").innerText = (`O valor de ${tempFahrenheit}°F em Celsius é ${valorCelsius}°C`); //Altera o valor do meu <p> através do seletor ".text" dentro do meu HTML para trazer a mensagem na página.
