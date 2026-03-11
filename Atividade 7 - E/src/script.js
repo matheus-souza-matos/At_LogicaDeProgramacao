92d5d22ea5eeabd24b391271f35315df623dfa69
Matheus <matheusmatos463463@gmail.com>
2026-03-11 17:19:01 -0300

    E - Valor parcela em atraso

--- Atividade 7 - E/src/script.js
+++ Atividade 7 - E/src/script.js
@@ -1,1 +1,12 @@
-// Efetuar o cálculo e a apresentação do valor de uma prestação em atraso, utilizando a fórmula PRESTACAO  VALOR + (VALOR * TAXA/100) * TEMPO).
+// Efetuar o cálculo e a apresentação do valor de uma prestação em atraso, utilizando a fórmula PRESTACAO <- VALOR + (VALOR * TAXA/100) * TEMPO).
+
+let valorPrestacao = parseFloat(prompt("Qual o valor da prestação?"));
+let taxa = parseFloat(prompt("Qual a porcentagem da taxa? (apenas números, sem a '%'"));
+let tempo = parseFloat(prompt("Quanto tempo (em dias) de atraso?"));
+
+let vPrestacaoAtraso = valorPrestacao + (valorPrestacao * taxa / 100) * tempo;
+
+alert(`
+    O valor da prestação de R$${valorPrestacao} com atraso de ${tempo} dias com taxa de ${taxa}%, é de:
+    R$${vPrestacaoAtraso};
+`);
