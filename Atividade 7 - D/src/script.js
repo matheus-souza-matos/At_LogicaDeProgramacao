df0536d268840b936a2bebc2a9f9560592bf4f02
Matheus <matheusmatos463463@gmail.com>
2026-03-11 16:53:31 -0300

    D - Calcular a quantidade de gasolina gasto em uma viagem

--- Atividade 7 - D/src/script.js
+++ Atividade 7 - D/src/script.js
@@ -1,1 +1,16 @@
-// Efetuar o cálculo da quantidade de litros de combustível gasta em uma viagem, utilizando um automóvel que faz 12 Km por litro. Para obter o cálculo, o usuário deve fornecer o tempo gasto (TEMPO) e a velocidade média (VELOCIDADE) durante a viagem. Desta forma, será possível obter a distância percorrida com a fórmula DISTANCIA  TEMPO * VELOCIDADE. Possuindo o valor da distância, basta calcular a quantidade de litros de combustível utilizada na viagem com a fórmula LITROS_USADOS  DISTANCIA / 12. Ao final, o programa deve apresentar os valores da velocidade média (VELOCIDADE), tempo gasto na viagem (TEMPO), a distancia percorrida (DISTANCIA) e a quantidade de litros (LITROS_USADOS) utilizada na viagem.
+// Efetuar o cálculo da quantidade de litros de combustível gasta em uma viagem, utilizando um automóvel que faz 12 Km por litro. Para obter o cálculo, o usuário deve fornecer o tempo gasto (TEMPO) e a velocidade média (VELOCIDADE) durante a viagem. Desta forma, será possível obter a distância percorrida com a fórmula DISTANCIA <- TEMPO * VELOCIDADE. Possuindo o valor da distância, basta calcular a quantidade de litros de combustível utilizada na viagem com a fórmula LITROS_USADOS <- DISTANCIA / 12. Ao final, o programa deve apresentar os valores da velocidade média (VELOCIDADE), tempo gasto na viagem (TEMPO), a distancia percorrida (DISTANCIA) e a quantidade de litros (LITROS_USADOS) utilizada na viagem.
+
+
+let tempo = parseFloat(prompt("Qual o tempo (em horas) da viagem?"));
+let vMedia = parseFloat(prompt("Qual velocidade média (em km/h) da viagem?"));
+
+let distancia = tempo * vMedia;
+let litrosUsados = distancia / 12;
+
+alert(`
+    Para um veículo de autonomia de 12km, o resultado foi:
+    Velocidade Média: ${vMedia} km/h;
+    Tempo da Viagem: ${tempo} hr(s);
+    Distância Percorrida: ${distancia} km;
+    Quantidade de Litros Usado: ${litrosUsados.toFixed(2)} L;
+`);
