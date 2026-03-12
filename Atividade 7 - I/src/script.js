36c99ad72e5afcbfcdf2f686cb4090b287b01bf1
Matheus <matheusmatos463463@gmail.com>
2026-03-12 12:49:10 -0300

    I - quadrado da diferença de A e B

--- Atividade 7 - I/src/script.js
+++ Atividade 7 - I/src/script.js
@@ -1,1 +1,12 @@
 // Ler dois inteiros (variáveis A e B) e imprimir o resultado do quadrado da diferença do primeiro valor pelo segundo.
+
+let numeroA = parseInt(prompt("Digite o primeiro numero inteiro: "));
+let numeroB = parseInt(prompt("Digite o segundo numero inteiro: "));
+
+let diferencaAB = numeroA - numeroB;
+let quadradoDiferenca = Math.pow(diferencaAB, 2);
+
+alert(`
+    A diferença de ${numeroA} para ${numeroB} é: ${diferencaAB};
+    O valor do quadrado dessa dirença (${diferencaAB}) é: ${quadradoDiferenca};    
+`);
