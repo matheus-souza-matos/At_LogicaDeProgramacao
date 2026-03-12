c18850715ffbe94e6c9e46d8ae706be87c10b665
Matheus <matheusmatos463463@gmail.com>
2026-03-12 17:53:53 -0300

    L - Soma dos quadrados dos 3 valores digitados

--- Atividade 7 - L/src/script.js
+++ Atividade 7 - L/src/script.js
@@ -1,1 +1,15 @@
 // Elaborar um programa que efetue a leitura de três valores (A, B e C) e apresente como resultado final à soma dos quadrados dos três valores lidos.
+
+let valor1 = parseFloat(prompt("Digite o primeiro valor:"));
+let valor2 = parseFloat(prompt("Digite o segundo valor:"));
+let valor3 = parseFloat(prompt("Digite o terceiro valor:"));
+
+let somaTotal = (valor1 ** 2) + (valor2 ** 2) + (valor3 ** 2 );
+
+alert(`
+    Primeiro Valor: ${valor1}; 
+    Segundo Valor: ${valor2};
+    Terceiro Valor: ${valor3};
+    A soma dos quadrados dos 3 valores é:
+    ${somaTotal}
+`);
