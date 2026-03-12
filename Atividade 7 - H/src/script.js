1549b0c1ed3a8ac7e4fce0f193fb59b230ac148a
Matheus <matheusmatos463463@gmail.com>
2026-03-12 12:24:07 -0300

    H - Volume caixa retangular

--- Atividade 7 - H/src/script.js
+++ Atividade 7 - H/src/script.js
@@ -1,1 +1,13 @@
-// Elaborar um programa que calcule e apresente o volume de uma caixa retangular, por meio da fórmula VOLUME  COMPRIMENTO * LARGURA * ALTURA.
+// Elaborar um programa que calcule e apresente o volume de uma caixa retangular, por meio da fórmula VOLUME <- COMPRIMENTO * LARGURA * ALTURA.
+
+let comprimentoCx = parseFloat(prompt("Qual o comprimento da caixa?"));
+let larguraCx = parseFloat(prompt("Qual a largura da caixa?"));
+let alturaCx = parseFloat(prompt("Qual a altura da caixa?"));
+
+if (comprimentoCx == larguraCx) {
+    alert("A caixa não é retangular!");
+    location.reload();
+}else {
+    let volume = comprimentoCx * larguraCx * alturaCx;
+    alert(`O volume da caixa retangular é: ${volume};`);
+}
