211fe7be3ea558136c1a80158e28363a0b551d85
Matheus <matheusmatos463463@gmail.com>
2026-03-13 09:51:35 -0300

    L - Soma dos quadrados dos 3 valores informados

--- Atividade 7 - L/src/script.js
+++ Atividade 7 - L/src/script.js
@@ -1,5 +1,6 @@
 // Elaborar um programa que efetue a leitura de três valores (A, B e C) e apresente como resultado final à soma dos quadrados dos três valores lidos.
 
+
 let valor1 = parseFloat(prompt("Digite o primeiro valor:"));
 let valor2 = parseFloat(prompt("Digite o segundo valor:"));
 let valor3 = parseFloat(prompt("Digite o terceiro valor:"));
