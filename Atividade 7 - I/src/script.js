56d10f26558862a7c708dea037fffcdb41818a41
Matheus <matheusmatos463463@gmail.com>
2026-03-13 09:34:42 -0300

    Quadrado da diferença de A e B

--- Atividade 7 - I/src/script.js
+++ Atividade 7 - I/src/script.js
@@ -3,8 +3,8 @@
 let numeroA = parseInt(prompt("Digite o primeiro numero inteiro: "));
 let numeroB = parseInt(prompt("Digite o segundo numero inteiro: "));
 
-let diferencaAB = numeroA - numeroB;
-let quadradoDiferenca = Math.pow(diferencaAB, 2);
+let diferencaAB = numeroA - numeroB; //Descobre a diferença dos dois número e armazena em "diferencaAB"
+let quadradoDiferenca = Math.pow(diferencaAB, 2); //Descobre o valor do quadrado de "diferencaAB" pelo método Math.pow
 
 alert(`
     A diferença de ${numeroA} para ${numeroB} é: ${diferencaAB};
