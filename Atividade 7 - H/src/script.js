03097f00b36fe34b5c6b82f83462b59b829b7d19
Matheus <matheusmatos463463@gmail.com>
2026-03-13 09:30:18 -0300

    Volume cx retangular

--- Atividade 7 - H/src/script.js
+++ Atividade 7 - H/src/script.js
@@ -4,10 +4,11 @@ let comprimentoCx = parseFloat(prompt("Qual o comprimento da caixa?"));
 let larguraCx = parseFloat(prompt("Qual a largura da caixa?"));
 let alturaCx = parseFloat(prompt("Qual a altura da caixa?"));
 
+// Condição criada para verificar se a caixa é ou não retangular
 if (comprimentoCx == larguraCx) {
     alert("A caixa não é retangular!");
-    location.reload();
-}else {
-    let volume = comprimentoCx * larguraCx * alturaCx;
+    location.reload(); // Recarrega a página caso a condição IF seja veradeira
+}else { 
+    let volume = comprimentoCx * larguraCx * alturaCx; // caso meu IF seja falso, ele retornará o volume da cx
     alert(`O volume da caixa retangular é: ${volume};`);
 }
